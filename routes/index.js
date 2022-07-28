24c3aaa4cf75cf17d4e57e64c345d6744a595f21
Vishal Jaiswal <vishaljaiswal@Vishals-MacBook-Pro.local>
2022-07-29 05:27:23 +0530

    Wrote code for deleting posts

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,5 @@ const homeControlle = require('../controllers/home_controller');
 router.get('/',homeControlle.home);
 router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
+router.use('/comments',require('./comments'));
 module.exports = router;
