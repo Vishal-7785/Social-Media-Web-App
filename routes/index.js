8f2eba4a3a642ac4d9187ce6a8cf4cdb2df21c3f
Vishal Jaiswal <vishaljaiswal@Vishals-MacBook-Pro.local>
2022-07-29 03:35:12 +0530

    Created post schema and displayed post of users

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,5 @@ const router = express.Router();
 const homeControlle = require('../controllers/home_controller');
 router.get('/',homeControlle.home);
 router.use('/users',require('./users'));
+router.use('/posts',require('./posts'));
 module.exports = router;
