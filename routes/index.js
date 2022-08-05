20d28de86f79b4586e30da4345400ed3a09b8de9
Vishal Jaiswal <vishaljaiswal@Vishals-MacBook-Pro.local>
2022-08-05 06:06:41 +0530

    Implemented like functionality on posts and comments

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ router.get('/',homeControlle.home);
 router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
 router.use('/comments',require('./comments'));
+router.use('/likes',require('./likes'));
 router.use('/api',require('./api'));
 module.exports = router;
